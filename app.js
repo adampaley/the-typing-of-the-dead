784fba1f5002d6608c485f09ae3797f3e30fc7ce
Adam Paley <apaley95@gmail.com>
2024-09-21 14:13:17 -0400

    adding game logic, pulling a random word and making sure it cannot be pulled again in the same game

---
+++ app.js
@@ -0,0 +1,57 @@
+let defeatedZombieWords = []
+
+let playerLevel = 1
+const winLevel = [10, 20, 30, 40]
+// pull word options from bank by level
+const wordObj = wordBank.find((wordLevel) => wordLevel.level === playerLevel)
+let wordList = wordObj.vocab
+let score = 0
+
+// randomize word selection, remove word from array so it does not generate again
+const getRandomWord = () => {
+    let zombieWord = wordList[Math.floor(Math.random() * wordList.length)]
+    let zombieWordIndex = wordList.indexOf(zombieWord)
+    wordList.splice(zombieWordIndex, 1)
+    return zombieWord
+}
+
+
+let zombieWord = getRandomWord()
+console.log(zombieWord)
+
+const killZombie = () => {
+    defeatedZombieWords.push(zombieWord)
+}
+
+killZombie()
+console.log(defeatedZombieWords)
+
+const completeLevel = () => {
+    if (defeatedZombieWords.length >= winLevel[playerLevel - 1]) {
+        return playerLevel++
+    }
+}
+completeLevel()
+console.log(playerLevel)
+
+const init = () => {
+    playerLevel = 0
+    score = 0
+}
+
+const reset = () => { // add event after building out html, cached elements, and dom
+    defeatedZombieWords.forEach((word) => { // adding these lines as a failsafe in case reseting without reloading does not put the words back into list
+        wordList.push(word)
+    })
+    defeatedZombieWords = []
+    init()
+}
+reset()
+console.log(defeatedZombieWords)
+console.log(wordList)
+// if I reset the game using a button, will the words return? 
+// as the zombies are defeated, might need to push them in an array of completed words and push(pop) them in at reset
+// also to clear that array when going to the next level
+
+
+
---
+++ word-bank.js
@@ -0,0 +1,7 @@
+const level1Words = ["variable", "theory", "significant", "principle", "period", "method", "interpretation", "indicate", "identify", "function", "formula", "factor", "evidence", "estimate", "distribution", "derive", "data", "consistent", "concept", "analyze"]
+const level2Words = ["ackowledge"]
+
+const wordBank = [
+    { level: 1, vocab: ["variable", "theory", "significant", "principle", "period", "method", "interpretation", "indicate", "identify", "function", "formula", "factor", "evidence", "estimate", "distribution", "derive", "data", "consistent", "concept", "analyze"]},
+    { level: 2, vocab: ["ackowledge"]}
+]
